39bc17e5865fc816a89c48d5dfcd339ad0508978
404ibra <ibra.aktass@gmail.com>
2023-04-03 16:10:45 +0300

    Project Created

---
+++ lib/views/auth/user_register.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserRegister extends StatelessWidget {
+  const UserRegister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
