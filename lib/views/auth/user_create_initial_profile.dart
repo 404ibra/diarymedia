213bf985dc44d4923268ddd3f5408a9acbb8f3b9
404ibra <ibra.aktass@gmail.com>
2023-04-04 17:16:16 +0300

    Create custom values

---
+++ lib/views/auth/user_create_initial_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateInitialProfile extends StatelessWidget {
+  const CreateInitialProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/views/auth/user_register.dart
+++ lib/views/auth/user_register.dart
@@ -1,5 +1,7 @@
+import 'package:dia/constant/custom_textstyles.dart';
 import 'package:dia/service/firebase_auth_service.dart';
 import 'package:dia/view_model/user_inputs.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class UserRegister extends StatelessWidget {
@@ -18,9 +20,25 @@ class UserRegister extends StatelessWidget {
               onPressed: () {
                 FirebaseFunctions().signInWithMail(
                     UserInputs.signInMail.controller.text,
-                    UserInputs.signInPassword.controller.text);
+                    UserInputs.signInPassword.controller.text,
+                    UserInputs.signInPasswordAgain.controller.text);
               },
-              child: Text("Kayıt ol"))
+              child: Text(
+                "Kayıt ol",
+                style: TextStyles.kTextStylePrimaryGreen
+                    .copyWith(fontSize: 21, fontWeight: FontWeight.bold),
+              )),
+          RichText(
+              text: TextSpan(children: [
+            const TextSpan(
+                text: "Zaten bir hesabınız var mı?",
+                style: TextStyles.kTextStylePrimaryGrey),
+            TextSpan(
+                text: " Giriş Yap",
+                style: TextStyles.kTextStylePrimaryGreen
+                    .copyWith(fontWeight: FontWeight.bold),
+                recognizer: TapGestureRecognizer()..onTap = () {})
+          ]))
         ],
       ),
     );
