f57aa47dd4519502c8f4baf8e97d8dab387da090
404ibra <ibra.aktass@gmail.com>
2023-04-14 23:58:33 +0300

    pick profile photo view model created

--- lib/views/auth/pick_profile_picture.dart
+++ lib/views/auth/pick_profile_picture.dart
@@ -1,13 +1,23 @@
+import 'package:dia/view_model/pick_image_boolen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 import '../../constant/constants.dart';
 
 class PickProfilePicture extends StatelessWidget {
-  const PickProfilePicture({super.key});
+  PickProfilePicture({super.key});
+  final ImagePicker picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
+    final pickedImageProvider = Provider.of<PickImageBoolenValue>(context);
+    Future<void> addPhoto() async {
+      final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+      pickedImageProvider.changePickedImagePath(pickedImage!.path);
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -20,6 +30,21 @@ class PickProfilePicture extends StatelessWidget {
                 style: TextStyles.kHeadlineTextStyle,
               ),
             ),
+            InkWell(
+              onTap: () async {
+                addPhoto();
+                pickedImageProvider.changeImagePickedStatus();
+              },
+              child: Container(
+                height: 120,
+                width: 120,
+                decoration:
+                    BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                child: pickedImageProvider.imagePicked
+                    ? Icon(Icons.person)
+                    : Image.file(pickedImageProvider.pickedImagePath),
+              ),
+            )
           ],
         ),
       ),
