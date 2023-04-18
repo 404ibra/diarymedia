2ca615391ed0071d3fd71249375fca5aaf496be0
404ibra <ibra.aktass@gmail.com>
2023-04-18 19:55:56 +0300

    home page ui created

--- lib/views/auth/home_page.dart
+++ lib/views/auth/home_page.dart
@@ -9,14 +9,50 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       bottomNavigationBar: NavBar(),
       body: SafeArea(
         child: Column(
-          children: const [
-            Center(
-              child: Text("sdkajdlksaj"),
-            )
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: size.height / 6,
+                      width: size.width,
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: FractionalOffset(0.0, 0.0),
+                              end: FractionalOffset(1.0, 0.0),
+                              stops: [
+                            0.0,
+                            .75
+                          ],
+                              colors: [
+                            Color.fromARGB(255, 87, 81, 255),
+                            CustomColors.primaryPurple,
+                          ])),
+                    ),
+                    Container(
+                      height: 20,
+                      width: size.width,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+                const Padding(
+                  padding: CustomPaddings.kHorizontalPadding,
+                  child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(
+                          "https://xsgames.co/randomusers/assets/avatars/pixel/6.jpg")),
+                )
+              ],
+            ),
           ],
         ),
       ),
