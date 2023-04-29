dea9f70368b2d5e9fe03e7fb8bf75d3b3b6fdd14
404ibra <ibra.aktass@gmail.com>
2023-04-29 23:46:57 +0300

    navbar bugs fixed

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,54 +1,118 @@
 import 'package:dia/constant/constants.dart';
+import 'package:dia/view_model/navbar_viewmodels.dart';
 import 'package:dia/views/daily_routine.dart';
 import 'package:dia/views/new_diary/new_diary_view.dart';
 import 'package:dia/widgets/navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<IconData> iconList = [
+      Icons.home,
+      Icons.search,
+      Icons.chat_bubble_outline_outlined,
+      Icons.person
+    ];
+    final navBarModel = Provider.of<NavBarViewModel>(context);
+
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: Icon(Icons.camera_alt),
-        title: Text(
-          'Routine',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: CircleAvatar(
+        radius: 30,
+        backgroundImage: AssetImage("assets/images/23.png"),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: const Offset(0, 10),
+              spreadRadius: 10)
+        ]),
+        child: BottomAppBar(
+          color: Colors.white,
+          shape: CircularNotchedRectangle(),
+          height: 75,
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  NavbarIcon(
+                    navBarModel: navBarModel,
+                    iconList: iconList,
+                    index: 0,
+                  ),
+                  NavbarIcon(
+                      navBarModel: navBarModel, iconList: iconList, index: 1),
+                  const SizedBox(),
+                  NavbarIcon(
+                      navBarModel: navBarModel, iconList: iconList, index: 2),
+                  NavbarIcon(
+                      navBarModel: navBarModel, iconList: iconList, index: 3),
+                ],
+              ),
+            ),
           ),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.send),
-            onPressed: () {},
-          ),
-        ],
       ),
-      bottomNavigationBar: NavBar(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               const Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0),
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8),
                 child: DiaryTopicList(),
               ),
-              ListView.separated(
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return const DailyRoutine();
-                  },
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 10),
-                  itemCount: 12)
+              SizedBox(
+                height: size.height,
+                child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: const [
+                          DailyRoutine(),
+                        ],
+                      );
+                    },
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(height: 10),
+                    itemCount: 12),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class NavbarIcon extends StatelessWidget {
+  NavbarIcon({
+    super.key,
+    required this.navBarModel,
+    required this.iconList,
+    required this.index,
+  });
+
+  final NavBarViewModel navBarModel;
+  final List<IconData> iconList;
+  int index;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => navBarModel.changeIndex(index),
+      child: Icon(
+        iconList[index],
+        size: 31,
+        color: navBarModel.selectionIndex == index
+            ? CustomColors.primaryPurple
+            : CustomColors.primaryGrey,
       ),
     );
   }
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -13,61 +13,58 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final navBarModel = Provider.of<NavBarViewModel>(context);
     final size = MediaQuery.of(context).size;
-    return SafeArea(
-      bottom: false,
-      child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          height: size.height * 0.1,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(color: Colors.white, boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                blurRadius: 5,
-                offset: const Offset(0, 10),
-                spreadRadius: 10)
-          ]),
-          child: ListView.builder(
-            itemCount: NavBarEnums.values.length,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return Row(
-                children: [
-                  InkWell(
-                    onTap: () => navBarModel.changeIndex(index),
-                    child: Column(
-                      children: [
-                        AnimatedContainer(
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          width: 50,
-                          margin: EdgeInsets.only(
-                            bottom: index == navBarModel.selectionIndex
-                                ? 0
-                                : size.width * .029,
-                            right: size.width * .0422,
-                            left: size.width * .0422,
-                          ),
-                          height: navBarModel.selectionIndex == index ? 2 : 0,
-                          decoration: const BoxDecoration(
-                              color: CustomColors.primaryPurple,
-                              borderRadius: BorderRadius.vertical(
-                                  bottom: Radius.circular(10))),
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        height: size.height * 0.1,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 5,
+              offset: const Offset(0, 10),
+              spreadRadius: 10)
+        ]),
+        child: ListView.builder(
+          itemCount: NavBarEnums.values.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                InkWell(
+                  onTap: () => navBarModel.changeIndex(index),
+                  child: Column(
+                    children: [
+                      AnimatedContainer(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.fastLinearToSlowEaseIn,
+                        width: 50,
+                        margin: EdgeInsets.only(
+                          bottom: index == navBarModel.selectionIndex
+                              ? 0
+                              : size.width * .029,
+                          right: size.width * .0422,
+                          left: size.width * .0422,
                         ),
-                        Icon(
-                          iconList[index],
-                          size: 31,
-                          color: navBarModel.selectionIndex == index
-                              ? CustomColors.primaryPurple
-                              : CustomColors.primaryGrey,
-                        ),
-                      ],
-                    ),
+                        height: navBarModel.selectionIndex == index ? 2 : 0,
+                        decoration: const BoxDecoration(
+                            color: CustomColors.primaryPurple,
+                            borderRadius: BorderRadius.vertical(
+                                bottom: Radius.circular(10))),
+                      ),
+                      Icon(
+                        iconList[index],
+                        size: 31,
+                        color: navBarModel.selectionIndex == index
+                            ? CustomColors.primaryPurple
+                            : CustomColors.primaryGrey,
+                      ),
+                    ],
                   ),
-                ],
-              );
-            },
-          )),
-    );
+                ),
+              ],
+            );
+          },
+        ));
   }
 
   List<IconData> iconList = [
