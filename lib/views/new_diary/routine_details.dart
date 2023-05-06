b271479e131ed448ffa6b7b0487b6a47990916a7
404ibra <ibra.aktass@gmail.com>
2023-05-06 23:57:09 +0300

    routine details screen has been started

--- lib/views/new_diary/routine_details.dart
+++ lib/views/new_diary/routine_details.dart
@@ -1,4 +1,8 @@
+import 'package:dia/constant/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../widgets/custom_bordered_button.dart';
 
 class RoutineDetails extends StatelessWidget {
   RoutineDetails({super.key, required this.index});
@@ -8,8 +12,42 @@ class RoutineDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const <Widget>[],
+      body: SafeArea(
+        child: Padding(
+          padding: CustomPaddings.kHorizontalPadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: CustomColors.primaryPurple,
+                      ),
+                    ),
+                    CustomBordererdButton(
+                      buttonText: "Devam Et",
+                      onTap: () {},
+                    )
+                  ]),
+              const SizedBox(height: 15),
+              Text(
+                "Yeni Bir Okuma Rutini Oluştur",
+                style: TextStyles.kHeadlineTextStyle.copyWith(fontSize: 20),
+              ),
+              const Text(
+                "Rutin için ilk detayları paylaş",
+                style: TextStyles.kTextStylePrimaryGrey,
+              ),
+              TextField(),
+            ],
+          ),
+        ),
       ),
     );
   }
