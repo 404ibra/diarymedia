b15659f1b3aa8c5c38065e8328a4bde0b6aeaf7b
404ibra <ibra.aktass@gmail.com>
2023-05-08 15:58:20 +0300

    scroll notification listener integrated profp.

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
 import 'package:dia/constant/constants.dart';
+import 'package:dia/widgets/profile_information_section.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -12,104 +11,29 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        toolbarHeight: 30,
+        toolbarHeight: 50,
         backgroundColor: Color(0xffffafcfe),
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back_ios_new_sharp,
+              color: CustomColors.profilePrimaryColor,
+            )),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.edit,
+                color: CustomColors.profilePrimaryColor,
+              )),
+        ],
       ),
       backgroundColor: Color(0xfffedf3fa),
       body: SafeArea(
         child: Column(
-          children: <Widget>[
-            Container(
-              height: size.height * 0.52,
-              decoration: const BoxDecoration(
-                  color: Color(0xffffafcfe),
-                  borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(15),
-                      bottomLeft: Radius.circular(15))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: CustomPaddings.kHorizontalPadding,
-                    child: Text(
-                      "Profilim",
-                      style: TextStyles.profileMainStyle,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 18),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 17.0),
-                          child: CircleAvatar(
-                            radius: 45,
-                            backgroundImage:
-                                AssetImage("assets/images/wash_face_img.png"),
-                          ),
-                        ),
-                        Text(
-                          "Jeniffer Gryhandell",
-                          style: TextStyles.profileMainStyle.copyWith(
-                              fontSize: 20, fontWeight: FontWeight.w600),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 8.0, bottom: 50),
-                          child: Text(
-                            "Reder, Fitness, Nutrion",
-                            style: TextStyle(
-                                color: Color(0xfffafb8cb),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 15),
-                          ),
-                        ),
-                        Padding(
-                          padding: CustomPaddings.kHorizontalPadding,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              ProfileDetailsRFF(
-                                headline: "Rutinler",
-                                number: "4",
-                              ),
-                              ProfileDetailsRFF(
-                                headline: "Takipler",
-                                number: "124",
-                              ),
-                              ProfileDetailsRFF(
-                                headline: "Takipçi",
-                                number: "3213",
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+          children: <Widget>[ProfileInformationSection(size: size)],
         ),
       ),
-    );
-  }
-}
-
-//RFF Mean is Routine Follower Follows
-class ProfileDetailsRFF extends StatelessWidget {
-  ProfileDetailsRFF({super.key, required this.headline, required this.number});
-
-  String headline;
-  String number;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(headline, style: TextStyles.rffHeadlineStyle),
-        Text(number, style: TextStyles.rffNumberStyle)
-      ],
     );
   }
 }
