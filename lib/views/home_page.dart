fe98bb62e7e472f2516969af5e2ac755ac81c612
404ibra <ibra.aktass@gmail.com>
2023-05-10 19:35:00 +0300

    some ui updates firebase integration

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dia/constant/custom_colors.dart';
 import 'package:dia/view_model/appbar_viewmodel.dart';
 import 'package:dia/widgets/custom_appbar.dart';
@@ -21,70 +22,85 @@ class HomePage extends StatelessWidget {
         ),
         body: Padding(
             padding: const EdgeInsets.all(10),
-            child: MasonryGridView.count(
-                crossAxisCount: 2,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8,
-                itemCount: posts.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onLongPress: () {},
-                    child: Column(children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Stack(
-                          alignment: Alignment.bottomCenter,
-                          children: [
-                            Image.network(
-                              posts[index].imageUrl,
-                              fit: BoxFit.cover,
-                            ),
-                            Container(
-                              height: 30,
-                              width: size.width / 2,
-                              color: Colors.white60,
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: 3.0, horizontal: 10),
-                                    child: CircleAvatar(
-                                      radius: 13,
-                                      backgroundImage: AssetImage(
-                                          "assets/images/wash_face_img.png"),
-                                    ),
-                                  ),
-                                  Column(
+            child: StreamBuilder(
+              stream:
+                  FirebaseFirestore.instance.collection("Routines").snapshots(),
+              builder: (context, snapshot) {
+                // print(snapshot.data!.docs[0].data());
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.data == null) {
+                  return Text("data");
+                }
+                final response = snapshot.data!;
+                return MasonryGridView.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                    itemCount: response.docs.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onLongPress: () {},
+                        child: Column(children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Stack(
+                              alignment: Alignment.bottomCenter,
+                              children: [
+                                Image.network(
+                                  response.docs[index]
+                                      ['routine_cover_image_path'],
+                                  fit: BoxFit.cover,
+                                ),
+                                Container(
+                                  height: 30,
+                                  width: size.width / 2,
+                                  color: Colors.white60,
+                                  child: Row(
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
-                                    children: const [
-                                      Text(
-                                        "ibra",
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.w600),
+                                    children: [
+                                      const Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 3.0, horizontal: 10),
+                                        child: CircleAvatar(
+                                          radius: 13,
+                                          backgroundImage: AssetImage(
+                                              "assets/images/wash_face_img.png"),
+                                        ),
                                       ),
-                                      Text(
-                                        "88.Gün",
-                                        style: TextStyle(
-                                            color: CustomColors
-                                                .profilePrimaryColor,
-                                            fontSize: 10,
-                                            fontWeight: FontWeight.w600),
-                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: const [
+                                          Text(
+                                            "ibra",
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                          Text(
+                                            "88.Gün",
+                                            style: TextStyle(
+                                                color: CustomColors
+                                                    .profilePrimaryColor,
+                                                fontSize: 10,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                        ],
+                                      )
                                     ],
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ]),
-                  );
-                })));
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ]),
+                      );
+                    });
+              },
+            )));
   }
 }
 
