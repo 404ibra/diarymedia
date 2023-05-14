0656b1eaad6c7adbfe8ff7b39136f37d1a6e95a0
404ibra <ibra.aktass@gmail.com>
2023-05-14 14:06:44 +0300

    routine view ui updates

--- lib/views/routine_view.dart
+++ lib/views/routine_view.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dia/constant/constants.dart';
 import 'package:dia/constant/custom_colors.dart';
 import 'package:dia/widgets/custom_appbar.dart';
 import 'package:dia/widgets/navbar.dart';
@@ -25,7 +27,51 @@ class RoutineView extends StatelessWidget {
         ),
       ),
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0, right: 8),
+                child: CircleAvatar(
+                  radius: 17,
+                  backgroundImage:
+                      AssetImage("assets/images/wash_face_img.png"),
+                ),
+              ),
+              StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection("Users")
+                      .doc(data['uid'])
+                      .snapshots(),
+                  builder: (context, userData) {
+                    if (userData.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (userData.data == null) {
+                      print("userdata.data is null");
+                    } else if (userData.hasError) {
+                      print("error var");
+                    }
+                    final userResponse = userData.data!.data();
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 1, bottom: 1.0),
+                          child: Text(userResponse!['user_name'],
+                              style: TextStyles.userNameUserNameStyle),
+                        ),
+                        Text("@${userResponse['user_username']}",
+                            style: TextStyles.userNameUserNameStyle.copyWith(
+                                color: CustomColors.profileSecondoryColor,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300)),
+                      ],
+                    );
+                  })
+            ],
+          )
+        ],
       ),
     );
   }
